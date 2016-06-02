eb8f8fd3f6ad06962fc58a6a97dfc30e63c7547b
Prakasa <na.raka45@gmail.com>
2016-06-03 05:53:21 +0700

    Notif With Toas BETA

--- app/src/main/java/com/terpusat/com/services/background/MainServices.java
+++ app/src/main/java/com/terpusat/com/services/background/MainServices.java
@@ -6,7 +6,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
-import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by prakasa on 03/06/16.
@@ -14,10 +14,8 @@ import android.util.Log;
 public class MainServices extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        super.onStartCommand(intent, flags, startId);
-
-        Log.i("LocalService : ", "Received start id : " + startId + " - Intens Prakasa : " + intent);
-        //stopSelf();
+        Toast.makeText(this,"Service created", Toast.LENGTH_LONG).show();
+        stopSelf();
 
         return START_STICKY;
     }
@@ -34,7 +32,7 @@ public class MainServices extends Service {
         AlarmManager alarm = (AlarmManager)getSystemService(ALARM_SERVICE);
         alarm.set(
                 alarm.RTC_WAKEUP,
-                System.currentTimeMillis() + (1 * 1 * 1),
+                System.currentTimeMillis() + (10 * 60 * 60),
                 PendingIntent.getService(this, 0, new Intent(this, MainServices.class), 0)
         );
     }
