66dc183831186cdd4692698ea2f606d5e9ea0440
Prakasa <na.raka45@gmail.com>
2016-06-03 06:34:59 +0700

    Add Notif Header Status

--- app/src/main/java/com/terpusat/com/services/background/MainServices.java
+++ app/src/main/java/com/terpusat/com/services/background/MainServices.java
@@ -1,12 +1,17 @@
 package com.terpusat.com.services.background;
 
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
-import android.widget.Toast;
+
+import com.terpusat.com.services.MainActivity;
+import com.terpusat.com.services.R;
 
 /**
  * Created by prakasa on 03/06/16.
@@ -14,7 +19,8 @@ import android.widget.Toast;
 public class MainServices extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this,"Service created", Toast.LENGTH_LONG).show();
+        //Toast.makeText(this,"Service created", Toast.LENGTH_LONG).show();
+        this.sendNotification();
         stopSelf();
 
         return START_STICKY;
@@ -32,8 +38,36 @@ public class MainServices extends Service {
         AlarmManager alarm = (AlarmManager)getSystemService(ALARM_SERVICE);
         alarm.set(
                 alarm.RTC_WAKEUP,
-                System.currentTimeMillis() + (10 * 60 * 60),
+                System.currentTimeMillis() + (1 * 60 * 60),
                 PendingIntent.getService(this, 0, new Intent(this, MainServices.class), 0)
         );
+    }
+
+    /* Cuztom Function */
+    private void sendNotification() {
+        Notification myNotication;
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification.Builder builder = new Notification.Builder(this);
+        NotificationManager  manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        builder.setAutoCancel(false);
+        builder.setTicker("Tracker Mode On");
+        builder.setContentTitle("Terpusat Status");
+        builder.setContentText("Anda sedang dalam mode GPS");
+        builder.setSmallIcon(R.drawable.ic_people);
+        builder.setContentIntent(resultPendingIntent);
+        builder.setOngoing(true);
+        builder.setSubText("Lihat selengkapnya...");   //API level 16
+        //builder.setNumber(100);
+        builder.build();
+
+        myNotication = builder.getNotification();
+        manager.notify(11, myNotication);
     }
 }
